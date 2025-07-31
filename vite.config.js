c9e961c7b8f785cc7097be7bc95b6ee4079aef4a
ldhldh07 <dhlim07@naver.com>
2025-07-31 17:39:42 +0900

    env: 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/front_6th_chapter2-1/',
+  build: {
+    rollupOptions: {
+      input: './index.advanced.html'
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
